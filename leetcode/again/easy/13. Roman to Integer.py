cb16511ce3907ca66ad2521a9a9c70c2ab203ea6
6691a <6691a@naver.com>
2022-06-08 21:36:24 +0900

    53. Maximum Subarray, again 53. Maximum Subarray

--- leetcode/again/easy/13. Roman to Integer.py
+++ leetcode/again/easy/13. Roman to Integer.py
@@ -1,3 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        
+        symbol = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+        value = 0
+        for i in range(len(s) -1):
+            current = s[i]
+            next = s[i + 1]
+            if symbol[current] < symbol[next]:
+                value -= symbol[current]
+            else:
+                value += symbol[current]
+        value += symbol[s[len(s) -1]]
+        return value
