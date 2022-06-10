1c5f07daa54cfa8fb5013793bd095df37100c9b6
6691a <6691a@naver.com>
2022-06-10 23:47:31 +0900

    again 14. Loest Common Prefix

--- leetcode/again/easy/14. Loest Common Prefix.py
+++ leetcode/again/easy/14. Loest Common Prefix.py
@@ -1,3 +1,14 @@
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
-        
+        answer = []
+        for x in zip(*strs):
+            print(x)
+            if len(set(x)) ==1:
+                answer.append(x[0])
+            else:
+                break
+        print(''.join(answer))
+        return ''.join(answer) 
+
+strs = ["flower", "flow", "flight"]
+Solution().longestCommonPrefix(strs)
