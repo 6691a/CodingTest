6f926d69260f55fac6d6fa2152bef89abdcd12c7
6691a <6691a@naver.com>
2022-06-18 20:48:21 +0900

    chaining hash table

---
+++ implement/hash table/chaining hash.py
@@ -0,0 +1,43 @@
+
+from calendar import c
+from curses import has_key
+
+
+class HashTable():
+    def __init__(self, size: int):
+        self.size = size
+        self.hash_table = [[] for i in range(size)]
+    
+    def add(self, key, value):
+        # hash_key =  hash(key)
+        hash_key =  key
+        target = self.hash_table[hash_key % self.size]
+        if target:
+            for i in range(len(target)):
+                if target[i][0] == hash_key:
+                    target[i][1] = value
+                    return
+                
+        target.append([hash_key, value])
+
+    
+    def get(self, key):
+        hash_key =  key
+        target = self.hash_table[hash_key % self.size]
+        if target:
+            for i in range(len(target)):
+                if target[i][0] == hash_key:
+                    print(target[i][1])
+                    return target[i][1]
+
+    def print(self):
+        print(self.hash_table)
+
+h = HashTable(5)
+h.add(1, 100)
+h.add(11, 200)
+h.add(111, 300)
+h.add(1111, 400)
+h.add(1, 10)
+
+# h.print()
